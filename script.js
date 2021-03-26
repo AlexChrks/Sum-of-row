207af6c2b7113d7eed257772c7ba04e435baad19
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-03-26 10:07:50 +0300

    refactor: refactor conditions, remove logs from the function

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 function range() {
   const memory = {};
-  return function (min,max) {
+  
+  return function (min, max) {
     
-    if (typeof min !== 'number' 
-      || typeof max !== 'number'   
+    if ( min < 1 
+      || max < 2
       || !Number.isInteger(min) 
       || !Number.isInteger(max)
-      || min < 1 
-      || max < 2) {
+) {
         throw new TypeError('Wrong range! Use positive numbers. First number in range should be less than second number.');
     }
     
-    if (memory[[min,max]]) {
-      console.log(`from memory: ${memory[[min,max]]}` );
-      return memory[[min,max]];
+    if (memory[[min, max]]) {
+      return memory[[min, max]];
     } else {
       let sumOfRange = ((max - min) + 1) * (min + max) / 2;
-      memory[[min,max]] = sumOfRange;
-      console.log(sumOfRange);
+      memory[[min, max]] = sumOfRange;
       return sumOfRange;  
     }
   }
@@ -25,8 +23,8 @@ function range() {
 
 const createRange = range();
 
-createRange(1,5);
-createRange(4,10);
-createRange(3,100);
-createRange(2,4)
-createRange(1,5);
+console.log(createRange(1,5));
+console.log(createRange(4,10));
+console.log(createRange(3,100));
+console.log(createRange(2,4));
+console.log(createRange(1,5));
